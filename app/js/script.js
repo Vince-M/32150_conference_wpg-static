84e3a63a9af374512ffaed6d02503f112c72555e
Vince Mendella <vmendella@gmail.com>
2022-07-08 06:25:59 -0500

    start writing the js for the dropdowns

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,30 @@
-console.log('Hello');
+console.log("Hello");
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+
+    /* Hamburger menu goes here */
+  }
+}
+
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+
+const items = document.querySelectorAll(".item");
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
