fa880fcc979bcf9e4845826cd31401f25aedb756
Vince Mendella <vmendella@gmail.com>
2022-07-14 14:10:37 -0500

    style dropdown menu for desktop

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,12 @@
 console.log("Hello");
 
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const nav__list = document.querySelector(".nav__list");
 const header__nav = document.querySelector(".header__nav");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 /* Toggle mobile menu */
 document.querySelector("#btnHamburger").addEventListener("click", function () {
@@ -12,9 +14,12 @@ document.querySelector("#btnHamburger").addEventListener("click", function () {
 
   if (header.classList.contains("open")) {
     // Close hamburger menu
+    body.classList.remove("noscroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
     nav__list.classList.add("nav__list");
     nav__list.classList.remove("nav__list--open");
     header__nav.classList.add("header__nav");
@@ -22,9 +27,12 @@ document.querySelector("#btnHamburger").addEventListener("click", function () {
     header__nav.classList.add("hide-for-mobile");
   } else {
     // Open hamburger menu
+    body.classList.add("noscroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
     nav__list.classList.add("nav__list--open");
     nav__list.classList.remove("nav__list");
     header__nav.classList.remove("header__nav");
