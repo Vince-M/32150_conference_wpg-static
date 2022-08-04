0a6db64246cc83301b9d3b4ab579229b6dc53be2
Vince Mendella <vmendella@gmail.com>
2022-08-04 17:15:52 -0500

    fix the dropdown menus so they now work on mobile. swictched some of the styles from max-width to min-width. Re-laid out sponsors section on index, added Meals sponsors

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 console.log("Hello");
+console.log("hi");
 
 const btnHamburger = document.querySelector("#btnHamburger");
 // const body = document.querySelector("body");
@@ -7,6 +8,8 @@ const overlay = document.querySelector(".overlay");
 const nav__list = document.querySelector(".nav__list");
 const header__nav = document.querySelector(".header__nav");
 const fadeElems = document.querySelectorAll(".has-fade");
+const menu = document.querySelector(".menu");
+const nav__items = document.querySelectorAll(".nav__item");
 
 /* Toggle mobile menu */
 document.querySelector("#btnHamburger").addEventListener("click", function () {
@@ -40,3 +43,33 @@ document.querySelector("#btnHamburger").addEventListener("click", function () {
     header__nav.classList.remove("hide-for-mobile");
   }
 });
+
+// Submenu
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+for (let nav__item of nav__items) {
+  if (nav__item.querySelector(".submenu")) {
+    nav__item.addEventListener("click", toggleItem, false);
+    nav__item.addEventListener("keypress", toggleItem, false);
+  }
+}
+
+// Close submenu from anywhere on page
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+
+document.addEventListener("click", closeSubmenu, false);
